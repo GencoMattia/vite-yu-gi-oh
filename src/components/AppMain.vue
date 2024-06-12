a6d93ad63843f4bc5c3b87c7f8753164b29d29cc
GencoMattia <gencomattia@hotmail.it>
2024-06-12 16:25:47 +0200

    added cards elements in appMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,60 @@
 <script>
+import axios from 'axios';
+
 export default {
     data() {
         return {
-        
+            cardsList: [
+
+            ],
         };
+    },
+
+    methods: {
+        getCards(){
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+            .then((response) => {
+                // handle success
+                console.log(response.data.data);
+                this.cardsList = response.data.data;
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+        }
+    },
+
+    created(){
+        this.getCards();
     }
 };
 </script>
 
 <template>
     <main>
-        <h1>
-            Sono un Main
-        </h1>
+        <section class="cards-wrapper">
+            <article v-for="card in cardsList" :key="card.id">
+                <div class="card" style="width: 18rem;">
+                    <img :src="card.card_images.image_url" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">
+                            {{ card.desc }}
+                        </p>
+                    </div>
+                </div>
+            </article>
+        </section>
     </main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use "../styles/partials/variables" as *;
 
+    main {
+        background-color: $appOrange;
+    }
 </style>
