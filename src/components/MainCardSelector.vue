453780d2990a6ced2f9f0d885cb3adb8281650cd
GencoMattia <gencomattia@hotmail.it>
2024-06-13 17:25:45 +0200

    created Main CardSelector component

---
+++ src/components/MainCardSelector.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+        
+        };
+    }
+
+    props: {
+        archetypeList: {
+            type: array,
+            required: true,
+        }
+    }
+};
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
